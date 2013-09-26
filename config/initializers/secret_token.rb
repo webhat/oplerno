d88ff969fdf6b2632ec3b579443a2391008961dd
Daniël W. Crompton <daniel.crompton@gmail.com>
2013-09-26 19:08:32 +0200

    Init Rails App

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Oplerno::Application.config.secret_key_base = '671fed24ccf0f5b7efda4d2c2b9bfa55127e443d15513077a1e0b385c605935d8411a81a86a139afddecc6e706834052369ba810774fe45b1a83e5786e6af754'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Oplerno::Application.config.session_store :cookie_store, key: '_oplerno_session'
