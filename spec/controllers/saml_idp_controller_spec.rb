3f6c4e5cbcb5288efdc009715dcd5f96ac7ee7a9
Daniël W. Crompton <daniel.crompton@gmail.com>
2013-12-16 02:23:54 +0100

    changed example.com to oplerno.com

--- spec/controllers/saml_idp_controller_spec.rb
+++ spec/controllers/saml_idp_controller_spec.rb
@@ -9,7 +9,7 @@ describe 'SamlIdpController' do
   end
 
   it "should find the SAML ACS URL" do
-    requested_saml_acs_url = "https://example.com/saml/consume"
+    requested_saml_acs_url = "https://oplerno.com/saml/consume"
     params[:SAMLRequest] = make_saml_request(requested_saml_acs_url)
     validate_saml_request
     saml_acs_url.should == requested_saml_acs_url
@@ -22,10 +22,10 @@ describe 'SamlIdpController' do
     end
 
     it "should create a SAML Response" do
-      saml_response = encode_SAMLResponse("foo@example.com")
+      saml_response = encode_SAMLResponse("foo@oplerno.com")
       response = Onelogin::Saml::Response.new(saml_response)
-      response.name_id.should == "foo@example.com"
-      response.issuer.should == "http://example.com"
+      response.name_id.should == "foo@oplerno.com"
+      response.issuer.should == "http://oplerno.com"
       response.settings = saml_settings
       response.is_valid?.should be_true
     end
@@ -33,10 +33,10 @@ describe 'SamlIdpController' do
     [:sha1, :sha256, :sha384, :sha512].each do |algorithm_name|
       it "should create a SAML Response using the #{algorithm_name} algorithm" do
         self.algorithm = algorithm_name
-        saml_response = encode_SAMLResponse("foo@example.com")
+        saml_response = encode_SAMLResponse("foo@oplerno.com")
         response = Onelogin::Saml::Response.new(saml_response)
-        response.name_id.should == "foo@example.com"
-        response.issuer.should == "http://example.com"
+        response.name_id.should == "foo@oplerno.com"
+        response.issuer.should == "http://oplerno.com"
         response.settings = saml_settings
         response.is_valid?.should be_true
       end
@@ -45,17 +45,17 @@ describe 'SamlIdpController' do
 
 	private
 
-  def make_saml_request(requested_saml_acs_url = "https://foo.example.com/saml/consume")
+  def make_saml_request(requested_saml_acs_url = "https://foo.oplerno.com/saml/consume")
     auth_request = Onelogin::Saml::Authrequest.new
     auth_url = auth_request.create(saml_settings(requested_saml_acs_url))
-    @request.host = 'example.com'
+    @request.host = 'oplerno.com'
     CGI.unescape(auth_url.split("=").last)
   end
 
-  def saml_settings(saml_acs_url = "https://foo.example.com/saml/consume")
+  def saml_settings(saml_acs_url = "https://foo.oplerno.com/saml/consume")
     settings = Onelogin::Saml::Settings.new
     settings.assertion_consumer_service_url = saml_acs_url
-    settings.issuer = "http://example.com/issuer"
+    settings.issuer = "http://oplerno.com/issuer"
     settings.idp_sso_target_url = "http://idp.com/saml/idp"
     settings.idp_cert_fingerprint = SamlIdp::Default::FINGERPRINT
     settings.name_identifier_format = SamlIdp::Default::NAME_ID_FORMAT
