02b726730b74b877bba2a426952458acbd7e5a20
Daniël W. Crompton <daniel.crompton@gmail.com>
2014-01-10 23:57:45 +0100

    copistrano test

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,20 +1,22 @@
 set :application, 'Oplerno'
+
+set :scm, :git
 set :repo_url, 'git@github.com:webhat/oplerno.git'
+set :branch, 'development'
 
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
 
-# set :deploy_to, '/var/www/my_app'
-# set :scm, :git
+set :deploy_to, '/home/redhat/www'
 
-# set :format, :pretty
-# set :log_level, :debug
+set :format, :pretty
+set :log_level, :debug
 # set :pty, true
 
 # set :linked_files, %w{config/database.yml}
 # set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
-# set :keep_releases, 5
+set :keep_releases, 4
 
 namespace :deploy do
 
