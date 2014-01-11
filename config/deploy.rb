59a0ea7701710a2772f3c4e6e371a550b3f52c8d
Daniël W. Crompton <daniel.crompton@gmail.com>
2014-01-12 00:50:50 +0100

    cap deploy fix

--- config/deploy.rb
+++ config/deploy.rb
@@ -27,7 +27,7 @@ namespace :deploy do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
       # Here we can do anything such as:
       within release_path do
-        execute :rake, 'cache:clear'
+        execute :echo, 'cache:clear'
       end
     end
   end
