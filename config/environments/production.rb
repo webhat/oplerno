8be16ce273b0e4b2e6507fa0237d1d3982c26354
Daniël W. Crompton <daniel.crompton@gmail.com>
2014-01-15 15:26:34 +0100

    changes for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,7 +5,7 @@ Oplerno::Application.configure do
   config.cache_classes = true
 
   # Full error reports are disabled and caching is turned on
-  config.consider_all_requests_local       = false
+  config.consider_all_requests_local = false
   config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
@@ -46,11 +46,11 @@ Oplerno::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w( devise_authy.js )
+  config.assets.precompile += %w( devise_authy.js main.js plugins.js bootstrap-responsive.css chardinjs-0.1.2.css magic-bootstrap.css main.css normalize.css )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_url_options = { :host => 'oplerno.com' }
+  config.action_mailer.default_url_options = {:host => 'oplerno.com'}
 
   # Enable threaded mode
   # config.threadsafe!
