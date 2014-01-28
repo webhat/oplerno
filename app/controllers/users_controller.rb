18147014e4de29cf7cd69d5eef76d165f8bb1263
Daniël W. Crompton <daniel.crompton@gmail.com>
2014-01-28 15:25:06 +0100

    UserController specs added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,27 +1,7 @@
 # Handles #User objects.
-class UsersController < ApplicationController
+class UsersController  < ApplicationController #< InheritedResources::Base #
   before_filter :set_user, only: [:show, :edit, :update, :destroy]
   before_filter :authenticate_user!, only: [:show, :edit, :update, :destroy]
-
-  # GET /users
-  # GET /users.json
-  def index
-    @users = User.all
-  end
-
-  # GET /users/1
-  # GET /users/1.json
-  def show
-  end
-
-  # GET /users/new
-  def new
-    @user = User.new
-  end
-
-  # GET /users/1/edit
-  def edit
-  end
 
   # POST /users
   # POST /users.json
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -7,6 +7,83 @@ describe UsersController do
 
   let(:valid_session) { {} }
 
-  describe 'there is no route to users'
+  def current_user_id
+    session['warden.user.user.key'][0][0]
+  end
+
+  def current_user
+    User.find(current_user_id)
+  end
+
+  context 'GET edit' do
+    context 'while signed in' do
+      login_user
+
+      it 'should show first_name' do
+        user = current_user
+        user.first_name = 'Test Name'
+        user.save
+        
+        get :edit, {id: current_user_id}
+        assigns(:user).encrypted_first_name.should eq current_user.encrypted_first_name
+      end
+      it 'should show email' do
+        get :edit, {id: current_user_id}
+        assigns(:user).email.should eq current_user.email
+      end
+    end
+    context 'while signed out' do
+      before do
+        @user = FactoryGirl.create(:user)
+      end
+      it 'should show email' do
+        get :edit, {id: @user.id}
+        assigns(:user).email.should eq @user.email
+      end
+    end
+  end
+
+  context 'PUT update' do
+    context 'while signed in' do
+			login_user
+      it 'should update first_name' do
+        user = current_user
+        user.first_name = 'Test Name'
+        user.save
+        
+				put :update, {id: user.to_param, user: {first_name: 'Other Name'}}
+				user.reload
+        assigns(:user).encrypted_first_name.should eq user.encrypted_first_name
+			end
+      it 'should update email' do
+        user = current_user
+        
+				put :update, {id: user.to_param, user: {email: 'test_user_controller@oplerno.com'}}
+				user.reload
+        assigns(:user).email.should eq user.email
+			end
+    end
+    context 'while signed out'
+  end
+
+  context 'GET show' do
+    context 'while signed in' do
+      login_user
+
+      it 'should show first_name' do
+        user = current_user
+        user.first_name = 'Test Name'
+        user.save
+        
+        get :show, {id: current_user_id}
+        assigns(:user).encrypted_first_name.should eq current_user.encrypted_first_name
+      end
+      it 'should show email' do
+        get :show, {id: current_user_id}
+        assigns(:user).email.should eq current_user.email
+      end
+    end
+    context 'while signed out'
+  end
 end
 
