d3d211c7946590b706ec9a14f6b6687a819ee57a
Daniël W. Crompton <daniel.crompton@gmail.com>
2014-02-04 01:32:19 +0100

    fixed capybara tests for new behaviour

--- spec/features/url_spec.rb
+++ spec/features/url_spec.rb
@@ -6,10 +6,11 @@ describe 'Visiting URLs' do
   end
 
   let(:valid_course) { {name: 'A course that cant be confused', price: '101'} }
+	let(:valid_user) { { title: 'King Maker', first_name: 'Check', last_name: 'Me', password: 'testtest', password_confirmation: 'testtest', email: 'teacher@oplerno.com' } }
 
   context 'while logged in' do
     before (:each) do
-			@user = FactoryGirl.build(:user, password: 'testtest')
+			@user = User.create! valid_user
 			@user.confirm!
 
 			valid_course[:teacher] = @user.id
@@ -21,6 +22,8 @@ describe 'Visiting URLs' do
 			click_button I18n.t('devise.sessions.new.sign_in')
       @course = Course.create! valid_course
 			@user.courses << @course
+
+			@user.save
     end
 
     after (:each) do
@@ -30,70 +33,70 @@ describe 'Visiting URLs' do
       Course.all.each { |course|
 				course.delete
 			}
+			@user.destroy
     end
 
     it 'should be able to visit the courses and pick a course' do
       visit '/courses'
-      expect(page).to have_content I18n.t('courses.view')
-      find(:xpath, "(//a[text()='#{I18n.t('courses.view')}'])[1]").click
+			page.first(".course > a").click
     end
     it 'should be able to visit the courses and register a course' do
       visit '/courses'
-      expect(page).to have_content I18n.t('courses.view')
-      find(:xpath, "(//a[text()='#{I18n.t('courses.view')}'])[1]").click
+			find('.course > a').click
 
       # Register Page
       expect(page).to have_content I18n.t('courses.register')
-      click_button I18n.t('courses.register')
+			find(:xpath, "//*[@value='#{I18n.t('courses.register')}']").click
 
 			visit '/carts/mycart'
       expect(page).to have_content valid_course[:name]
     end
     it 'should be able to visit the courses and pick a teacher' do
       visit '/courses'
-      expect(page).to have_content I18n.t('courses.view')
-      find(:xpath, "(//a[text()='#{I18n.t('courses.view')}'])[1]").click
+			page.first(".course > a").click
 
       # View Teacher
 			expect(page).to have_content @user.encrypted_first_name
-			find("div.teacher_image_small").click
+			find(".teacher > a").click
 
       expect(page).to have_content @user.encrypted_first_name
       expect(page).to have_content @user.encrypted_last_name
     end
     it 'should be able to visit the teachers and pick a course' do
-      visit '/courses'
-      expect(page).to have_content I18n.t('courses.view')
-      find(:xpath, "(//a[text()='#{I18n.t('courses.view')}'])[1]").click
+      visit '/teachers'
+			page.first('.teacher > a').click
 
-      # View Teacher
-      expect(page).to have_content I18n.t('courses.view')
-      click_link I18n.t('courses.view')
+      # View Course
+      expect(page).to have_content @course.name
+			page.first(".course > a").click
 
-      expect(page).to have_content @user.encrypted_first_name
-      expect(page).to have_content @user.encrypted_last_name
+      expect(page).to have_content @course.name
     end
     it 'should be able to visit the teachers and pick a teacher' do
 			visit '/teachers/'
       expect(page).to have_content @user.encrypted_first_name
       expect(page).to have_content @user.encrypted_last_name
-      find(:xpath, "(//a[text()='#{I18n.t('users.view')}'])[1]").click
+			page.first(".teacher > a").click
 
       expect(page).to have_content @user.encrypted_first_name
       expect(page).to have_content @user.encrypted_last_name
 		end
     it 'should be able to visit the cart and remove a course' do
       visit '/courses'
-      find(:xpath, "(//a[text()='#{I18n.t('courses.view')}'])[1]").click
-      click_button I18n.t('courses.register')
+			page.first(".course > a").click
+
+      # Register Page
+      expect(page).to have_content valid_course[:name]
+      expect(page).to have_content I18n.t('courses.register')
+			find(:xpath, "//*[@value='#{I18n.t('courses.register')}']").click
 
 			visit '/carts/mycart'
       expect(page).to have_content valid_course[:name]
-			page.all('.col-6').count.should eq 1
+			page.all('.col-6').count.should eq 2
 
 			click_button I18n.t('cart.remove')
 			expect(page).to have_content I18n.t('cart.removed', {course: valid_course[:name]})
-			page.all('.col-6').count.should eq 0
+			page.all('.col-6').count.should eq 1
 		end
   end
 end
