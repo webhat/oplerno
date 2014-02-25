e179a008708b1739a5cadc21b36b61a71f5ea693
Daniël W. Crompton <daniel.crompton@gmail.com>
2014-02-25 11:58:27 +0100

    cart controller fix to be inline with the hariki advisory #19

--- spec/controllers/carts_controller_spec.rb
+++ spec/controllers/carts_controller_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe CartsController do
   login_user
 
-  let(:valid_attributes) { {user_id: current_user.id} }
+  let(:valid_cart) { {} }
 
   let(:valid_course) { {name: 'The Orange Theme'} }
 
@@ -12,11 +12,20 @@ describe CartsController do
     session[:course_id] = course.id
   end
 
+	before do
+		@cart = Cart.create! valid_cart
+		@cart.user = current_user
+		@cart.save
+	end
+
+	after do
+		@cart.destroy
+	end
+
   describe "GET show" do
     it "assigns the requested cart as @cart" do
-      cart = Cart.create! valid_attributes
-      get :show, {:id => cart.to_param}
-      assigns(:cart).should eq(cart)
+      get :show, {:id => @cart.to_param}
+      assigns(:cart).should eq(@cart)
     end
   end
 
@@ -29,22 +38,25 @@ describe CartsController do
 
   describe "GET edit" do
     it "assigns the requested cart as @cart" do
-      cart = Cart.create! valid_attributes
-      get :edit, {:id => cart.to_param}
-      assigns(:cart).should eq(cart)
+      get :edit, {:id => @cart.to_param}
+      assigns(:cart).should eq(@cart)
     end
   end
 
   describe "POST create" do
+		before do
+			@cart.destroy
+		end
+
     describe "with valid params" do
       it "creates a new Cart" do
         expect {
-          post :create, {:cart => valid_attributes}
+          post :create, {:cart => valid_cart}
         }.to change(Cart, :count).by(1)
       end
 
       it "assigns a newly created cart as @cart" do
-        post :create, {:cart => valid_attributes}
+        post :create, {:cart => valid_cart}
         assigns(:cart).should be_a(Cart)
         assigns(:cart).should be_persisted
       end
@@ -63,62 +75,51 @@ describe CartsController do
   describe "PUT update" do
     describe "with valid params" do
       it "updates the requested cart" do
-        cart = Cart.create! valid_attributes
-        # Assuming there are no other carts in the database, this
-        # specifies that the Cart created on the previous line
-        # receives the :update_attributes message with whatever params are
-        # submitted in the request.
         Cart.any_instance.should_receive(:update_attributes).with({"these" => "params"})
-        put :update, {:id => cart.to_param, :cart => {"these" => "params"}}
+        put :update, {:id => @cart.to_param, :cart => {"these" => "params"}}
       end
 
       it "assigns the requested cart as @cart" do
-        cart = Cart.create! valid_attributes
-        put :update, {:id => cart.to_param, :cart => valid_attributes}
-        assigns(:cart).should eq(cart)
+        put :update, {:id => @cart.to_param, :cart => valid_cart}
+        assigns(:cart).should eq(@cart)
       end
 
       it "redirects to the cart" do
-        cart = Cart.create! valid_attributes
-        put :update, {:id => cart.to_param, :cart => valid_attributes}
-        response.should redirect_to(cart)
+        put :update, {:id => @cart.to_param, :cart => valid_cart}
+        response.should redirect_to(@cart)
       end
     end
 
     describe "with invalid params" do
       it "assigns the cart as @cart" do
-        cart = Cart.create! valid_attributes
         # Trigger the behavior that occurs when invalid params are submitted
         Cart.any_instance.stub(:save).and_return(false)
-        put :update, {:id => cart.to_param, :cart => {}}
-        assigns(:cart).should eq(cart)
+        put :update, {:id => @cart.to_param, :cart => {}}
+        assigns(:cart).should eq(@cart)
       end
     end
   end
 
   describe "DELETE destroy" do
     it "destroys the requested cart" do
-      cart = Cart.create! valid_attributes
       expect {
-        delete :destroy, {:id => cart.to_param}
+        delete :destroy, {:id => @cart.to_param}
       }.to change(Cart, :count).by(-1)
     end
 
     it "redirects to the carts list" do
-      cart = Cart.create! valid_attributes
-      delete :destroy, {:id => cart.to_param}
+      delete :destroy, {:id => @cart.to_param}
       response.should redirect_to(courses_url)
     end
   end
 
   describe 'post Remove from cart' do
     it 'remove' do
-      cart = Cart.create! valid_attributes
-      course = cart.courses.create! valid_course
+      course = @cart.courses.create! valid_course
       expect {
-        post :remove_course_from_cart, {cart: cart.id, course: course.id}
-      }.to change(cart.courses, :count).by(-1)
-      response.should redirect_to(cart)
+        post :remove_course_from_cart, {cart: @cart.id, course: course.id}
+      }.to change(@cart.courses, :count).by(-1)
+      response.should redirect_to(@cart)
       flash[:notice].should eq (I18n.t 'cart.removed', {course: course.name})
     end
   end
