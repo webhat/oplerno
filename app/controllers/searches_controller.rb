edc2ff7dc3d809dfa94cffe143f1fe64e0d321a2
Daniël W. Crompton <daniel.crompton@gmail.com>
2014-03-10 23:46:14 +0100

    cleaned up search errors

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -4,13 +4,14 @@ class SearchesController < ApplicationController
 	end
 
 	def create
-		@searches = Course.search params[:search][:term]
+		@search = params[:search][:term]
+		@searches = Course.search @search
 		not_found
 	end
 
 	private
 
 	def not_found
-		flash[:alert] = t('search.not_found') if @searches.empty?
+		flash[:alert] = t('search.not_found', term: @search) if @searches.empty?
 	end
 end
