90ecfc3e995f3d29bdbb37eee3bfea401317f3db
Daniël W. Crompton <daniel.crompton@gmail.com>
2014-05-22 12:00:47 +0200

    Fixed migration issues

--- db/migrate/20140521230529_add_secret_more_columns_to_order_transaction.rb
+++ db/migrate/20140521230529_add_secret_more_columns_to_order_transaction.rb
@@ -1,4 +1,4 @@
-class AddMoreSecretColumnsToOrderTransaction < ActiveRecord::Migration
+class AddSecretMoreColumnsToOrderTransaction < ActiveRecord::Migration
   def change
     add_column :order_transactions, :params_completed, :binary
     add_column :order_transactions, :params_completed_key, :binary
