b802ed3206b2941535722844aff68a31cf6b22bc
Daniël W. Crompton <daniel.crompton@gmail.com>
2014-06-09 16:33:40 +0200

    order transaction throws errors, log and swallow

--- app/admin/order_transaction.rb
+++ app/admin/order_transaction.rb
@@ -3,20 +3,35 @@ ActiveAdmin.register OrderTransaction do
   index do
     column :success
     column 'Order Value' do |order_transaction|
-      Cart.find(order_transaction.order_cart_id).total_price
+			begin
+				order_transaction.order.cart.total_price
+			rescue => e
+				p e
+				'?'
+			end
     end
     column :message
     column :courses do |order_transaction|
-      table_for order_transaction.order.cart.courses do
-        column do |course|
-          link_to course.name, [:admin, course]
-        end
-      end
+			begin
+				table_for order_transaction.order.cart.courses do
+					column do |course|
+						link_to course.name, [:admin, course]
+					end
+				end
+			rescue => e
+				p e
+				'?'
+			end
     end
     column 'User' do |order_transaction|
+			begin
       cart = order_transaction.order.cart
       user = User.find(cart.user_id)
       "#{user.encrypted_first_name} #{user.encrypted_last_name} (#{cart.user_id})"
+			rescue => e
+				p e
+				'?'
+			end
     end
   end
 end
