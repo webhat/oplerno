3b6b3e575b47fc9646fe6f991fd999aa20bbdad3
Daniël W. Crompton <daniel.crompton@gmail.com>
2014-07-28 13:17:19 +0200

    fixes staging and production updating

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,7 @@ set :rails_env, 'production'
 
 set :scm, :git
 set :repo_url, 'git@github.com:webhat/oplerno.git'
-set :branch, 'master'
+set :branch, 'develop'
 
 set :deploy_to, '/home/redhat/www'
 
