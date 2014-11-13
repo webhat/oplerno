a74019fcee205e075bed94ed05e388d958107650
Daniël W. Crompton <daniel.crompton@gmail.com>
2014-11-13 21:13:48 +0100

    changes to controller [ci skip]

--- spec/controllers/searches_controller_spec.rb
+++ spec/controllers/searches_controller_spec.rb
@@ -1,5 +1,14 @@
 require 'spec_helper'
 
 describe SearchesController do
-
+	context 'when I search for something' do
+		it 'should return something' do
+			pending 'setup searchkick on Travis CI'
+			get :index, {}
+		end
+		it 'should return something' do
+			pending 'setup searchkick on Travis CI'
+			get :create, { search: { term: 'test' } }
+		end
+	end
 end
