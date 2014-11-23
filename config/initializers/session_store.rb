67ab22e4e27172719e5b2ad8e13b942f7f696dfa
Daniël W. Crompton <daniel.crompton@gmail.com>
2014-11-23 20:03:33 +0100

    Fix for redis session storage

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,4 +1,4 @@
 # Be sure to restart your server when you modify this file.
 
 #Oplerno::Application.config.session_store :cookie_store, key: '_oplerno_session'
-Oplerno::Application.config.session_store :redis_store, :servers => "redis://puppet:6379/10"
+Oplerno::Application.config.session_store :redis_store, :servers => "#{ENV['PAPERCLIP_REDIS']}/10"
