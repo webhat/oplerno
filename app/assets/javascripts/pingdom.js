433e114bb4a43e5e2f5594346e14f5c519a33442
Daniël W. Crompton <daniel.crompton@gmail.com>
2015-01-09 00:04:05 +0100

    Added pingdom

---
+++ app/assets/javascripts/pingdom.js
@@ -0,0 +1,9 @@
+var _prum = [['id', '54ae88feabe53d8570ab65cd'],
+             ['mark', 'firstbyte', (new Date()).getTime()]];
+(function() {
+    var s = document.getElementsByTagName('script')[0]
+      , p = document.createElement('script');
+    p.async = 'async';
+    p.src = '//rum-static.pingdom.net/prum.min.js';
+    s.parentNode.insertBefore(p, s);
+})();
