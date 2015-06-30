1219b70888ad6dd0cd39779fd2a8cbd22b2f1c1a
Daniël W. Crompton <daniel.crompton@gmail.com>
2015-06-30 15:58:49 +0200

    Adding to cart updates Cart Button

--- app/assets/javascripts/angular/carts/carts_content_controller.js
+++ app/assets/javascripts/angular/carts/carts_content_controller.js
@@ -12,22 +12,30 @@
     return CartsContentIO;
   }]);
   window.oplernoApp.controller('CartsContentController', [
+    '$rootScope',
     '$scope',
     'CartsContentIO',
-    function($scope, CartsContentIO) {
+    function($rootScope, $scope, CartsContentIO) {
       $scope.items = 0;
       $scope.total = 0;
 
-      CartsContentIO.query().$promise.then(function(result) {
-        console.log('LEN: '+ result);
-        $scope.items = result.length;
-        $scope.total = 0;
-
-
-        for(i=0;i<result.length;i++){
-          $scope.total += result[i].price;
-        }
-
+      $rootScope.$on("cartContentUpdated", function (args) {
+        $scope.update();
       });
+
+      $scope.update = function() {
+        CartsContentIO.query().$promise.then(function(result) {
+          console.log('LEN: '+ result);
+          $scope.items = result.length;
+          $scope.total = 0;
+
+          for(i=0;i<result.length;i++){
+            $scope.total += result[i].price;
+          }
+
+        });
+      }
+
+      $scope.update();
   }]);
 }).call(this);
