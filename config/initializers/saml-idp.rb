78e9ef5b21c5cda4eea476231df9100abe884d65
Daniël W. Crompton <daniel.crompton@gmail.com>
2015-08-18 21:57:18 +0200

    Patch from @instructure
    Signed-off-by: Daniël W. Crompton <daniel.crompton@gmail.com>

--- config/initializers/saml-idp.rb
+++ config/initializers/saml-idp.rb
@@ -1,2 +1,2 @@
-SamlIdp.config.x509_certificate = Base64.encode64 File.open(Rails.root.join('config', 'saml', 'saml.pub')) { |file| file.read }
+SamlIdp.config.x509_certificate = File.read(Rails.root.join('config', 'saml', 'saml.pub')).gsub(/\w*-+(BEGIN|END) CERTIFICATE-+\w*/, "").strip
 SamlIdp.config.secret_key = File.open(Rails.root.join('config', 'saml', 'saml.pem')) { |file| file.read }
